47c4be440b40d550e4e7121aa790e9c03d3bb29c
JuanCamilo <juancmilo222@gmial.com>
2024-11-03 20:54:09 -0500

    change logic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookListComponent } from './components/book-list/book-list.component';
-import { AddBookComponent } from './components/add-book/add-book.component'; 
-import { EditBookComponent } from './components/edit-book/edit-book.component';
-import { AuthMiddleware } from './services/middleware/auth.middleware';
-import { LayoutComponent } from './layout/layout/layout.component';
-import { RegisterComponent } from './pages/auth/register/register.component';
-import { LoginComponent } from './pages/auth/login/login.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  {
-    path: 'dashboard',
-    component: LayoutComponent,
-    canActivate: [AuthMiddleware],
-    children: [
-      { path: 'books', component: BookListComponent }, 
-      { path: 'books/add', component: AddBookComponent },
-      { path: 'books/edit/:id', component: EditBookComponent }
-    ]
-  },
-  { path: '**', redirectTo: '/login' }
-];
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component'; 
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { AuthMiddleware } from './services/middleware/auth.middleware';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'books', component: BookListComponent }, 
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'dashboard',
+    component: LayoutComponent,
+    canActivate: [AuthMiddleware],
+    children: [
+      { path: 'books', component: BookListComponent }, 
+      { path: 'books/add', component: AddBookComponent },
+      { path: 'books/edit/:id', component: EditBookComponent }
+    ]
+  },
+  { path: '**', redirectTo: '/login' }
 ];
